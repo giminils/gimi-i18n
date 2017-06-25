f0751000769c765e40766b595de80497f14c7c41
andersback <anders.back@me.com>
2017-06-25 10:17:01 +0200

    kjdf

--- __tests__/GoogleTranslateTests.js
+++ __tests__/GoogleTranslateTests.js
@@ -1,13 +1,12 @@
 jest.disableAutomock()
-var translate = require('@google-cloud/translate')({
-  projectId: 'gimi-969a7',
-  keyFilename: './utils/gkey/Gimi-b4f63676ca99.json'
-})
 
 describe('AnnaHelper', () => {
   xit('it should be able to hash string', () => {
+    var translate = require('@google-cloud/translate')({
+      projectId: 'gimi-969a7',
+      keyFilename: './utils/gkey/Gimi-b4f63676ca99.json'
+    })
     translate.translate('<span class="notranslate">{goal.updated_by.name}</span> har aktiverat sparmål <span class="notranslate">{goal.title}</span>.', 'sv')
-      .then((err, translation) => {
-      })
+      .then((err, translation) => {})
   })
 })
