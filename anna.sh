347858cb8ba42b506e8d9506113c6fa476e528e5
andersback <anders.back@me.com>
2017-07-31 12:54:41 +0200

    more seo texts

--- anna.sh
+++ anna.sh
@@ -1,16 +1,26 @@
 #run this file by running ./anna.sh in your terminal
-npm run anna -- seo.home.h1
-npm run anna -- seo.home.h2
-npm run anna -- seo.home.h3
-npm run anna -- seo.family.h1
-npm run anna -- seo.family.h2
-npm run anna -- seo.family.h3
-npm run anna -- seo.training.h1
-npm run anna -- seo.training.h2
-npm run anna -- seo.training.h3
-npm run anna -- seo.about.h1
-npm run anna -- seo.about.h2
-npm run anna -- seo.about.h3
-npm run anna -- seo.news.h1
-npm run anna -- seo.news.h2
-npm run anna -- seo.news.h3
+npm run anna seo.home.h1
+npm run anna seo.home.h2
+npm run anna seo.home.h3
+npm run anna seo.home.title
+npm run anna seo.home.description
+npm run anna seo.family.h1
+npm run anna seo.family.h2
+npm run anna seo.family.h3
+npm run anna seo.family.title
+npm run anna seo.family.description
+npm run anna seo.training.h1
+npm run anna seo.training.h2
+npm run anna seo.training.h3
+npm run anna seo.training.title
+npm run anna seo.training.description
+npm run anna seo.about.h1
+npm run anna seo.about.h2
+npm run anna seo.about.h3
+npm run anna seo.about.title
+npm run anna seo.about.description
+npm run anna seo.news.h1
+npm run anna seo.news.h2
+npm run anna seo.news.h3
+npm run anna seo.news.title
+npm run anna seo.news.description
