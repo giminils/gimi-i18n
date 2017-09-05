2acdbc64ff5398d621ba8e450338306e1beaff58
SuperDuperMartin <martinagnalv@gmail.com>
2017-09-05 17:01:56 +0200

    started with tests

---
+++ __tests__/SuggestedAllowanceTests.js
@@ -0,0 +1,21 @@
+import SuggestedAllowance from '../SuggestedAllowance'
+// import ExchangeRates from '../ExchangeRates'
+import 'babel-polyfill'
+
+jest.disableAutomock()
+
+describe('default', () => {
+  it('', () => {
+    // expect(getSupportedCurrencyInfos()).not.toEqual(undefined)
+  })
+
+  it('SE', () => {
+
+  })
+
+  it('all currencies in Default CurrencyCodes needs to be in supported Currency Codes', () => {
+    Object.keys(SuggestedAllowance).map((country) => {
+      // console.log(SuggestedAllowance[country])
+    })
+  })
+})
