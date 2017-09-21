8acd4d05e19a9c80af8a400bced32953d9d26522
anback <anders.back@me.com>
2017-09-21 16:02:38 +0200

    fixed JSON test in gimi-web

--- __tests__/JSONTests.js
+++ __tests__/JSONTests.js
@@ -5,7 +5,9 @@
  let templateDirs = ['./text_strings/client', './text_strings/server', './text_strings/templates', './text_strings/gimi-web', './text_strings/share-image-generator']
 
  describe('JSON files', () => {
+   it('LOL')
    templateDirs.forEach((dir) => {
+     if(!fs.existsSync(dir)) return
      fs.readdirSync(dir).forEach((file) => {
        let path = `${dir}/${file}`
        it(`it should have valid JSON for ${path}`, () => {
