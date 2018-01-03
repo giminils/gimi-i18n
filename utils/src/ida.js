cc20acae8cffa8d81c176ea42dc9a169fe8a5a52
hstamenov3 <hristo.stamenov3@gmail.com>
2018-01-03 12:29:56 +0200

    IDR config

--- utils/src/ida.js
+++ utils/src/ida.js
@@ -3,7 +3,7 @@
 var fs = require('fs')
 
 let RunIda = (): * => {
-  var files = ['config_AUD.json', 'config_CAD.json', 'config_DEF.json', 'config_DKK.json', 'config_EUR.json', 'config_GBP.json', 'config_INR.json', 'config_ISK.json', 'config_NOK.json', 'config_NZD.json', 'config_SEK.json', 'config_THB.json', 'config_USD.json']
+  var files = ['config_AUD.json', 'config_CAD.json', 'config_DEF.json', 'config_DKK.json', 'config_EUR.json', 'config_GBP.json', 'config_INR.json', 'config_ISK.json', 'config_NOK.json', 'config_NZD.json', 'config_SEK.json', 'config_THB.json', 'config_USD.json',  'config_IDR.json']
   files.forEach((file) => {
     syncConfigKeys(file)
   })
