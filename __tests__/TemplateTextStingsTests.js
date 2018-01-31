dcf639b496c0f1e09f2b33c548a3d62c4781ddef
Arvin Behshad <arvin@gimitheapp.com>
2018-01-31 17:00:56 +0100

    it:ed all previous x:its

--- __tests__/TemplateTextStingsTests.js
+++ __tests__/TemplateTextStingsTests.js
@@ -16,7 +16,7 @@ var checkForPattern = (patternString: string): boolean => {
 }
 
 describe('default', () => {
-  xit('Should have _description for _name or _title', () => {
+  it('Should have _description for _name or _title', () => {
     var errors = []
 
     var langKeys = Object.keys(svLang)
