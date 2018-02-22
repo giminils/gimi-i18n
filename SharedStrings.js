b440cb04213c8039048c69d10fa79e57fb518b6a
anback <anders.back@me.com>
2018-02-22 12:35:01 +0100

    rearranged stuff

--- SharedStrings.js
+++ SharedStrings.js
@@ -14,49 +14,12 @@ import * as I18n from './index'
 // import de from './text_strings/shared/de.json'
 // import et from './text_strings/shared/et.json'
 
-export let getSharedStrings = (lang: string) => {
-  return en
-  /* switch (lang.substring(0, 2)) {
-    case 'da' : return da // danish
-    case 'sv' : return sv // sweden
-    case 'nb' :
-    case 'nn' : return no // norway
-    case 'fr' : return fr // france
-    case 'nl' : return nl // Netherlands
-    case 'be' : return be // belgian
-    case 'fi' : return fi // finish
-    case 'it' : return it // italian
-    case 'es' : return es // spanish
-    case 'de' : return de // german
-    case 'is' : return is // island
-    case 'et' : return et // Estonia
-    default : return en
-  } */
-}
-
-export function getText (textid: *, values?: Array<*>, textStrings: *): string {
-  if (textStrings === undefined) return ''
-  if (!textStrings || !textid) return ''
-  var text = textStrings[textid]
-  if (!text) return ''
-  else text = I18n.removeTranslationHelpers(text)
-  text = text.trim()
-  if (values)
-    values.forEach((item, index) => {
-      text = text.split(`%${index + 1}$s`).join(getText(item)) // TextStrings
-      // $FlowFixMe //Needed 05.12.2017
-      text = text.split(`%${index + 1}$d`).join(item) // Digits
-    })
-  text = text.charAt(0).toUpperCase() + text.slice(1)
-  return text
-}
-
-export let getCardQuestion = (step: number, lang: string): string => {
+export let getCardQuestion = (step: number, lang: string = "en"): string => {
   let textStrings = getSharedStrings(lang)
   return getText(`card_test_question_${step}`, [], textStrings)
 }
 
-export let getCardAnswer = (step: number, lang: string): Array<Object> => {
+export let getCardAnswer = (step: number, lang: string = "en"): Array<Object> => {
   let textStrings = getSharedStrings(lang)
   let answers = []
   for (var i = 0; i < 3; i++) answers.push({title: getText(`card_test_question_${step}_answer_${i + 1}`, [], textStrings), valid: getValidCardAnswer(step, i)})
@@ -89,4 +52,39 @@ let getValidCardAnswer = (step: number, answer: number): boolean => {
       return true
     default: return false
   }
+}
+
+let getSharedStrings = (lang: string) => {
+  return en
+  /* switch (lang.substring(0, 2)) {
+    case 'da' : return da // danish
+    case 'sv' : return sv // sweden
+    case 'nb' :
+    case 'nn' : return no // norway
+    case 'fr' : return fr // france
+    case 'nl' : return nl // Netherlands
+    case 'be' : return be // belgian
+    case 'fi' : return fi // finish
+    case 'it' : return it // italian
+    case 'es' : return es // spanish
+    case 'de' : return de // german
+    case 'is' : return is // island
+    case 'et' : return et // Estonia
+    default : return en
+  } */
+}
+
+let getText = (textid: *, values?: Array<*>, textStrings: *): string => {
+  if (textStrings === undefined) return ''
+  if (!textStrings || !textid) return ''
+  var text = textStrings[textid]
+  if (!text) return ''
+  else text = I18n.removeTranslationHelpers(text)
+  text = text.trim()
+  if (values)
+    values.forEach((item, index) => {
+      text = text.split(`%${index + 1}$s`).join(getText(item)) // TextStrings
+    })
+  text = text.charAt(0).toUpperCase() + text.slice(1)
+  return text
 }
