f9ff566092507df12e682d4d82e77d603ba9055b
SuperDuperMartin <martinagnalv@gmail.com>
2018-04-20 11:04:02 +0200

    added nb.json

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // @flow
 import sv from './text_strings/moonshine/sv.json'
 import en from './text_strings/moonshine/en.json'
+import no from './text_strings/moonshine/nb.json'
 import da from './text_strings/client/da.json'
 import fi from './text_strings/client/fi.json'
 import is from './text_strings/client/is.json'
 import fr from './text_strings/client/fr.json'
 import nl from './text_strings/client/nl.json'
-import no from './text_strings/client/nb.json'
 import be from './text_strings/client/be.json'
 import it from './text_strings/client/it.json'
 import es from './text_strings/client/es.json'
