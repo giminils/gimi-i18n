bb3df4a66fc35d2531c99332025e27b0b8817fb3
hstamenov3 <hristo.stamenov3@gmail.com>
2018-04-24 13:33:02 +0300

    fix lint

--- utils/src/taylor.js
+++ utils/src/taylor.js
@@ -6,33 +6,33 @@
 //  Output: A stringified JSON object
 //
 
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 const fs = require('fs')
-const filename = process.argv[2];
-const prefix = process.argv[3];
+const filename = process.argv[2]
+const prefix = process.argv[3]
 
 if (process.argv.length < 3) {
-    console.log('Usage: npm run taylor -- {file} [prefix]]');
-    process.exit(1);
+  console.log('Usage: npm run taylor -- {file} [prefix]]')
+  process.exit(1)
 }
 
 fs.readFile(filename, 'utf8', function (err, data) {
-    if (err)
-        throw err;
+  if (err)
+    throw err
 
-    let strings = data.split(/\r?\n/);
-    let ids     = data.toLowerCase().split(/\r?\n/);
-    let output  = {};
+  let strings = data.split(/\r?\n/)
+  let ids = data.toLowerCase().split(/\r?\n/)
+  let output = {}
 
-    for (var n = 0; n < ids.length; n++) {
-        let id = ids[n].split(' ').join('_').replace(/\W/g, '').replace(/_+$/, '');
-        if (id.charAt(0) === '_')
-            id = id.substr(1);
+  for (var n = 0; n < ids.length; n++) {
+    let id = ids[n].split(' ').join('_').replace(/\W/g, '').replace(/_+$/, '')
+    if (id.charAt(0) === '_')
+      id = id.substr(1)
 
-        if (prefix)
-            id = prefix + '_' + id;
-
-        output[id] = strings[n];
-    }
-    console.log(JSON.stringify(output, undefined, 2));
-    console.log('------------------');
-});
+    if (prefix)
+      id = prefix + '_' + id
+    output[id] = strings[n]
+  }
+  console.log(JSON.stringify(output, undefined, 2))
+  console.log('------------------')
+})
