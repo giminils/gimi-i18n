d28131b9db5f326169de8a8565e5302099960bee
Arvin Behshad <arvin@gimitheapp.com>
2018-04-24 12:52:19 +0200

    more fix

--- __tests__/GoogleTranslateTests.js
+++ __tests__/GoogleTranslateTests.js
@@ -1,12 +1,12 @@
+var translate = require('@google-cloud/translate')({
+  projectId: 'gimi-969a7',
+  keyFilename: './utils/gkey/Gimi-b4f63676ca99.json'
+})
 
 jest.disableAutomock()
 
 describe('AnnaHelper', () => {
   it('it should be able to hash string', () => {
-    var translate = require('@google-cloud/translate')({
-      projectId: 'gimi-969a7',
-      keyFilename: './utils/gkey/Gimi-b4f63676ca99.json'
-    })
     translate.translate('<span class="notranslate">{goal.updated_by.name}</span> har aktiverat sparmål <span class="notranslate">{goal.title}</span>.', 'sv')
       .then((err, translation) => {
         if (err) throw err
