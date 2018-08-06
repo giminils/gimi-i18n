7b777efbcac6615c9c061731f55d3ee0af407b41
hstamenov3 <hristo.stamenov3@gmail.com>
2018-08-06 14:59:12 +0300

    lili copy_text

---
+++ utils/src/Lili.js
@@ -0,0 +1,57 @@
+
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+var fs = require('fs')
+let templateDir = ['./text_strings/server', './text_strings/templates', './text_strings/gimi-web', './text_strings/client', './text_strings/bot']
+
+let RunLili = (filePath): * => {
+  let getPath = (file) => `${filePath}/${file}`
+  var langPath = getPath('en.json') // Edit here for what language to use
+  var lang = fs.readFileSync(langPath, {encoding: 'utf8'})
+  lang = JSON.parse(lang)
+
+  let syncTextStrings = (file) => {
+    if (file.indexOf('.json') === -1)
+      return
+    if (file === 'default.json') return
+    if (file === 'lang.json')
+      return
+    var path = getPath(file)
+    var TextStrings = fs.readFileSync(path, {encoding: 'utf8'})// Textstings file
+    TextStrings = JSON.parse(TextStrings)
+
+    // Craete Support§
+    let NewTextStrings = {...lang}
+    Object.keys(lang).forEach(key => {
+      if (lang[key].includes('PLZ_COPY') && !file.includes('en.json')) {
+        if (!file.includes('sv.json'))
+          NewTextStrings[key] = NewTextStrings[key].replace('PLZ_COPY', 'PLZ_TRANSLATE')
+        NewTextStrings[key] = NewTextStrings[key].replace('PLZ_COPY', 'PLZ_CHECK')
+        TextStrings[key] = `${NewTextStrings[key]}`
+      }
+    })
+
+    NewTextStrings = {...NewTextStrings, ...TextStrings}
+
+    var NewTextStringsLength = Object.keys(NewTextStrings).length
+    var TextStringsLength = Object.keys(TextStrings).length
+    var delta = NewTextStringsLength - TextStringsLength
+
+    if (delta > 0)
+      console.log(`Updated ${delta} textstrings in ${file}`)
+
+    // Save changes
+    NewTextStrings = JSON.stringify(NewTextStrings, undefined, 2)
+    fs.unlinkSync(path)
+    fs.writeFileSync(path, NewTextStrings, {encoding: 'utf8'})
+  }
+
+  fs.readdirSync(filePath).forEach((languageCode) => syncTextStrings(languageCode))
+
+  // fix swedish TextStrings formatting
+  lang = JSON.stringify(lang, undefined, 2)
+  fs.unlinkSync(langPath)
+  fs.writeFileSync(langPath, lang, {encoding: 'utf8'})
+}
+templateDir.forEach((filePath) => {
+  RunLili(filePath)
+})
