4ed42e3a6d6765a32d7636d3cd7843110e0f21aa
Martin <martinagnalv@gmail.com>
2018-09-19 13:51:42 +0200

    testsFix

--- __tests__/JSONTests.js
+++ __tests__/JSONTests.js
@@ -6,7 +6,7 @@ jest.disableAutomock()
 let templateDirs = ['./text_strings/client', './text_strings/server', './text_strings/templates', './text_strings/gimi-web', './text_strings/share-image-generator']
 
 describe('JSON files', () => {
-  it('LOL')
+  it('LOL', () => {})
   templateDirs.forEach((dir) => {
     if (!fs.existsSync(dir)) return
     fs.readdirSync(dir).forEach((file) => {
