be3c0507031de141d0bb99fcb4b697a16c4b80ba
Mathias <mathias@barnpengar.se>
2018-11-14 14:21:29 +0100

    Add education subtitle strings and reorder for design

--- Courses.js
+++ Courses.js
@@ -6,97 +6,9 @@ let defaultChapter = {
 export default [
   {
     ...defaultCourse,
-    name: 'save',
-    titleLangKey: 'course_save_title',
-    subtitleLangKey: '',
-    chapters: [
-      {
-        ...defaultChapter,
-        chapterID: 'save_chapter_1',
-        chapterName: 'Intro to interest',
-        titleLangKey: 'save_chapter_1',
-        content: {
-          chapterIntroLangKey: 'save_chapter_1_intro',
-          chapterSessionDescLangKey: 'save_chapter_1_desc',
-          slideContent: [
-            {
-              titleLangKey: 'save_chapter_1_step_1_title',
-              descLangKey: 'save_chapter_1_step_1_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_1_step_2_title',
-              descLangKey: 'save_chapter_1_step_2_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_1_step_3_title',
-              descLangKey: 'save_chapter_1_step_3_desc'
-            }
-          ]
-        }
-      },
-      {
-        ...defaultChapter,
-        chapterID: 'save_chapter_2',
-        chapterName: 'Cutting back on sweets',
-        titleLangKey: 'save_chapter_2',
-        content: {
-          chapterIntroLangKey: 'save_chapter_2_intro',
-          chapterSessionDescLangKey: 'save_chapter_2_desc',
-          slideContent: [
-            {
-              titleLangKey: 'save_chapter_2_step_1_title',
-              descLangKey: 'save_chapter_2_step_1_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_2_step_2_title',
-              descLangKey: 'save_chapter_2_step_2_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_2_step_3_title',
-              descLangKey: 'save_chapter_2_step_3_desc'
-            }
-          ]
-        }
-      },
-      {
-        ...defaultChapter,
-        chapterID: 'save_chapter_3',
-        chapterName: 'Toilet paper challenge',
-        titleLangKey: 'save_chapter_3',
-        content: {
-          chapterIntroLangKey: 'save_chapter_3_intro',
-          chapterSessionDescLangKey: 'save_chapter_3_desc',
-          slideContent: [
-            {
-              titleLangKey: 'save_chapter_3_step_1_title',
-              descLangKey: 'save_chapter_3_step_1_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_3_step_2_title',
-              descLangKey: 'save_chapter_3_step_2_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_3_step_3_title',
-              descLangKey: 'save_chapter_3_step_3_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_3_step_4_title',
-              descLangKey: 'save_chapter_3_step_4_desc'
-            },
-            {
-              titleLangKey: 'save_chapter_3_step_5_title',
-              descLangKey: 'save_chapter_3_step_5_desc'
-            }
-          ]
-        }
-      }
-    ]
-  },
-  {
-    ...defaultCourse,
     name: 'earn',
     titleLangKey: 'course_earn_title',
-    subtitleLangKey: '',
+    subtitleLangKey: 'course_earn_subtitle',
     chapters: [
       {
         ...defaultChapter,
@@ -186,9 +98,97 @@ export default [
   },
   {
     ...defaultCourse,
+    name: 'save',
+    titleLangKey: 'course_save_title',
+    subtitleLangKey: 'course_save_subtitle',
+    chapters: [
+      {
+        ...defaultChapter,
+        chapterID: 'save_chapter_1',
+        chapterName: 'Intro to interest',
+        titleLangKey: 'save_chapter_1',
+        content: {
+          chapterIntroLangKey: 'save_chapter_1_intro',
+          chapterSessionDescLangKey: 'save_chapter_1_desc',
+          slideContent: [
+            {
+              titleLangKey: 'save_chapter_1_step_1_title',
+              descLangKey: 'save_chapter_1_step_1_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_1_step_2_title',
+              descLangKey: 'save_chapter_1_step_2_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_1_step_3_title',
+              descLangKey: 'save_chapter_1_step_3_desc'
+            }
+          ]
+        }
+      },
+      {
+        ...defaultChapter,
+        chapterID: 'save_chapter_2',
+        chapterName: 'Cutting back on sweets',
+        titleLangKey: 'save_chapter_2',
+        content: {
+          chapterIntroLangKey: 'save_chapter_2_intro',
+          chapterSessionDescLangKey: 'save_chapter_2_desc',
+          slideContent: [
+            {
+              titleLangKey: 'save_chapter_2_step_1_title',
+              descLangKey: 'save_chapter_2_step_1_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_2_step_2_title',
+              descLangKey: 'save_chapter_2_step_2_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_2_step_3_title',
+              descLangKey: 'save_chapter_2_step_3_desc'
+            }
+          ]
+        }
+      },
+      {
+        ...defaultChapter,
+        chapterID: 'save_chapter_3',
+        chapterName: 'Toilet paper challenge',
+        titleLangKey: 'save_chapter_3',
+        content: {
+          chapterIntroLangKey: 'save_chapter_3_intro',
+          chapterSessionDescLangKey: 'save_chapter_3_desc',
+          slideContent: [
+            {
+              titleLangKey: 'save_chapter_3_step_1_title',
+              descLangKey: 'save_chapter_3_step_1_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_3_step_2_title',
+              descLangKey: 'save_chapter_3_step_2_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_3_step_3_title',
+              descLangKey: 'save_chapter_3_step_3_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_3_step_4_title',
+              descLangKey: 'save_chapter_3_step_4_desc'
+            },
+            {
+              titleLangKey: 'save_chapter_3_step_5_title',
+              descLangKey: 'save_chapter_3_step_5_desc'
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    ...defaultCourse,
     name: 'spend',
     titleLangKey: 'course_spend_title',
-    subtitleLangKey: '',
+    subtitleLangKey: 'course_spend_subtitle',
     chapters: [
       {
         ...defaultChapter,
