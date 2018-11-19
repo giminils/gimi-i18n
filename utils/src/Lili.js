92b7351366cc19c83a622cabf0b641d356c686ef
anback <anders.back@me.com>
2018-11-19 15:06:48 +0100

    Update utils/src/Lili.js

--- utils/src/Lili.js
+++ utils/src/Lili.js
@@ -1,7 +1,7 @@
 
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 var fs = require('fs')
-let templateDir = ['./text_strings/server', './text_strings/templates', './text_strings/gimi-web', './text_strings/client', './text_strings/bot']
+let templateDir = ['./text_strings/server', './text_strings/templates', './text_strings/gimi-web', './text_strings/client', './text_strings/bot', './text_strings/education']
 let PLZ_CHECK = 'PLZ_CHECK'
 let PLZ_COPY = 'PLZ_COPY'
 let PLZ_TRANSLATE = 'PLZ_TRANSLATE'
