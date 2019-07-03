816ab2b2473fb9c8cfef988fe0fa124907b43847
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-07-03 16:57:06 +0200

    downgrade to match app

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
   let presets = []
   api.cache(true)
     presets = [
-      '@babel/preset-env',
-      'module:metro-react-native-babel-preset'
+      '@babel/preset-env'
     ]
   const plugins = [
     '@babel/plugin-transform-flow-strip-types'
