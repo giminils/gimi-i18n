f3d6bb3527150b1a68592a34e813500011cd7e27
Martin Agnälv <martinagnalv@gmail.com>
2020-03-03 15:16:46 +0100

    texts

--- __tests__/TextStringsForClient.tests.js
+++ __tests__/TextStringsForClient.tests.js
@@ -30,7 +30,7 @@ describe('TextStrings', () => {
         return split[0]
       })
       oldStructKeys.forEach(oldKey => {
-        if (newStructKeys.indexOf(oldKey) === -1) console.warn('key: ' + oldKey + ' is missing in new_structure')
+        if (newStructKeys.indexOf(oldKey) === -1) console.warn('key: ' + oldKey + ' is missing in new_structure in file' + ' ' + lang + '.json')
         expect(newStructKeys.indexOf(oldKey) !== -1).toEqual(true)
         // if (oldKey === 'hej') console.warn(newStruct[oldKey])
         /* if (typeof newStruct[oldKey] === 'object')
