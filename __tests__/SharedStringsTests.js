ecc4f15c8d02ea4f6d369cb4669eb15418c14526
hstamenov3 <hristo.stamenov3@gmail.com>
2020-03-16 16:17:57 +0200

    should not have FIT

--- __tests__/SharedStringsTests.js
+++ __tests__/SharedStringsTests.js
@@ -5,7 +5,7 @@ import {getCurrencyConfig} from '../lib/CurrencyConfig'
 jest.disableAutomock()
 
 describe('SharedStringsTests', () => {
-  fit('it should onvert $c{whatever} to correct currency', () => {
+  test('it should onvert $c{whatever} to correct currency', () => {
     let question = getFinLitTestQuestion(1, 3, 'en', getCurrencyConfig('SEK'))
     expect(!question.includes('$c{')).toBeTruthy()
   })
