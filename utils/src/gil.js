61e6a483e504e40b731020e4a347722858b50c72
onurersel <onurersel@gmail.com>
2020-03-23 14:46:45 +0100

    gil fixes files

--- utils/src/gil.js
+++ utils/src/gil.js
@@ -1,14 +1,19 @@
 // checks if langKeys are pairing with new i18n structure
+// options:
+// --fix: Saves files with correct translation path
 
 const fs = require('fs')
 
 const enJson = require('../../../i18n/text_strings/client_new_structure/en.json')
 const foldersToCheck = ['../components', '../hocs', '../libs', '../reducers']
+const endMessages = []
+const shouldFix = process.argv.filter(el => el === '--fix').length > 0
 
 const getKeyMap = (json, path, keys = {}) => {
   Object.keys(json).forEach(key => {
     if (typeof json[key] === 'string') {
-      keys[key] = !path ? key : `${path}.${key}`
+      if (!keys[key]) keys[key] = []
+      keys[key].push(!path ? key : `${path}.${key}`)
       return
     }
     getKeyMap(json[key], key, keys)
@@ -17,18 +22,31 @@ const getKeyMap = (json, path, keys = {}) => {
 }
 
 const keyMap = getKeyMap(enJson)
-let hasError = false
 
-const findLangKeysInFile = (filePath, langKey, fullLangKey) => {
+
+const findLangKeysInFile = (filePath, langKey, fullLangKeys) => {
   const fileString = fs.readFileSync(filePath, {encoding: 'utf8'})
   const matches = fileString.match(new RegExp(`'\w*.{0,1}${langKey}'`, 'g'))
   if (!matches) return
   matches.forEach(el => {
-    const res = el.match(fullLangKey)
-    if (!res) {
-      console.log(`${filePath}: \`${el}\` should be \`${fullLangKey}\``)
-      hasError = true
+    let didMatch = false
+    let i = 0
+    while (!didMatch && i < fullLangKeys.length) {
+      const res = el.match(fullLangKeys[i])
+      if (res) didMatch = true
+      i++
     }
+    if (didMatch) return
+    if (!shouldFix && fullLangKeys.length === 1) return console.log(`\`${filePath}\`: \`${el}\` should be \`'${fullLangKeys[0]}'\``)
+    if (!shouldFix) return console.log(`\`${filePath}\`: \`${el}\` should be one of \`${fullLangKeys.join(', ')}\``)
+    if (fullLangKeys.length > 1) {
+      const message = `Cannot fix \`${filePath}\`: Please manually fix \`${el}\` with one of \`${fullLangKeys.join(', ')}\``
+      endMessages.push(message)
+      return console.log(message)
+    }
+    console.log(`Fixing \`${filePath}\`: \`${el}\` -> \`'${fullLangKeys[0]}'\`...`)
+    const newFileString = fileString.replace(new RegExp(`'${langKey}'`, 'g'), `'${fullLangKeys[0]}'`)
+    fs.writeFileSync(filePath, newFileString, {encoding: 'utf8'})
   })
 }
 
@@ -45,7 +63,12 @@ const checkFolder = (folderPath, keyMap) => {
   })
 }
 
-foldersToCheck.forEach(folderPath => {
-  checkFolder(folderPath, keyMap)
-  if (!hasError) console.log('No errors!')
-})
+const checkAllFolders = () => {
+  foldersToCheck.forEach(folderPath => {
+    checkFolder(folderPath, keyMap)
+  })
+  console.log(`\n\n\nFinished with ${endMessages.length} warnings.`)
+  console.log(endMessages.join('\n'))
+}
+
+checkAllFolders()
