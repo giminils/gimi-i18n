57cf21d85a924a3f6cb645c753c7f847fb8cc190
Martin Agnälv <martinagnalv@gmail.com>
2020-05-06 09:49:49 +0200

    JSOn tests from gimi-app too

--- __tests__/JSONTests.ts
+++ __tests__/JSONTests.ts
@@ -5,12 +5,19 @@ import jsonValidator from 'json-dup-key-validator'
 jest.disableAutomock()
 
 describe('JSON files', () => {
-  it('LOL', () => {})
-  let dirs = fs.readdirSync('./text_strings/')
+  it('LOL', () => { })
+
+  let dirs
+  try {
+    dirs = fs.readdirSync('./text_strings/')
+  }
+  catch (e) {
+    dirs = fs.readdirSync('./i18n/text_strings/')
+  }
   dirs.forEach((dir: string) => {
     if (!fs.existsSync(dir)) return
     try {
-      if (dir === '.DS_Store') return
+      if (dir === '.DS_Store' || dir === 'ios') return
       fs.readdirSync(dir).forEach((file: string) => {
         if (file === '.DS_Store') return
         const path = `${dir}/${file}`
