a77f2f526b79914bf5be9bdbec5b65240b71e7a0
Calle Mannervik <c.mannervik@gmail.com>
2021-01-07 11:58:07 +0100

    Get available chapters

--- school/School.ts
+++ school/School.ts
@@ -9,32 +9,36 @@ export const chapters = Chapters.data
 export const lessons = Lessons.data
 
 export let getAllLessons = () => {
-    return lessons.map((lesson) => {
-        let story = stories.find((story) => story.id === lesson.storyId)
-        let challange = challanges.find((challange) => challange.id === lesson.challangeId)
-        const {id, type, isEnabled, titleLangKey, subtitleLangKey,imageUrl} = lesson
-        return {id, type, isEnabled, titleLangKey, subtitleLangKey, imageUrl, story, challange}
-    })
+  return lessons.map((lesson) => {
+    let story = stories.find((story) => story.id === lesson.storyId)
+    let challange = challanges.find((challange) => challange.id === lesson.challangeId)
+    const {id, type, isEnabled, titleLangKey, subtitleLangKey, imageUrl} = lesson
+    return {id, type, isEnabled, titleLangKey, subtitleLangKey, imageUrl, story, challange}
+  })
 }
 
 export let getLesson = (id: number) => {
-    let lessons = getAllLessons()
-    return lessons.find((lesson) => lesson.id === id)
+  let lessons = getAllLessons()
+  return lessons.find((lesson) => lesson.id === id)
 }
 
 export let getAllChapters = () => {
-    return chapters
+  return chapters
 }
 
 export let getAllLessonsInChapter = (id: number) => {
-    let chapter = chapters.find((chapter) => chapter.id === id)
-    let lessons = getAllLessons()
-    return lessons.filter((lesson) => {
-        if (!chapter || !chapter.lessonIds) return []
-        return chapter.lessonIds.indexOf(lesson.id) !== -1
-    })
+  let chapter = chapters.find((chapter) => chapter.id === id)
+  let lessons = getAllLessons()
+  return lessons.filter((lesson) => {
+    if (!chapter || !chapter.lessonIds) return []
+    return chapter.lessonIds.indexOf(lesson.id) !== -1
+  })
 }
 
 export let getChapter = (id: number) => {
-    return chapters.find((chapter) => chapter.id === id)
+  return chapters.find((chapter) => chapter.id === id)
+}
+
+export let getAvailableChapters = () => {
+  return chapters.filter((chapter) => !chapter.isLocked)
 }
