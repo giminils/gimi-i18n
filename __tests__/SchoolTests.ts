e969942db0e539c430aca23174a98196dffb49a6
Calle Mannervik <c.mannervik@gmail.com>
2021-01-15 14:26:34 +0100

    School tests improvments

--- __tests__/SchoolTests.ts
+++ __tests__/SchoolTests.ts
@@ -20,49 +20,32 @@ describe('School tests', () => {
     stories.map((story) => ids.push(story.id))
     expect([...new Set(ids)].length === ids.length).toBeTruthy()
   })
-  test('stories should have multiple screens', () => {
-    stories.map((story) => {
-      const {screens} = story
+  stories.map((story) => {
+    const {screens} = story
+    test('stories should have multiple screens', () => {
       if (!screens || screens.length === 0) warnStoryId(story, 'screens length')
       expect(screens.length).toBeGreaterThan(0)
     })
-  })
-  test('story screen should have one of: titleLangKey, subTitleLangKey', () => {
-    stories.map((story) => {
-      story.screens.map((screen) => {
-        const {titleLangKey, subTitleLangKey} = screen
+    screens.map((screen) => {
+      const {type, titleLangKey, subTitleLangKey, buttons} = screen
+      test('story screen should have an existing type', () => {
+        if (!type || STORYSCREEN_TYPES.indexOf(type) === -1) warnStoryId(story, 'type')
+        expect(STORYSCREEN_TYPES.indexOf(type)).toBeGreaterThan(-1)
+      })
+      test('story screen should have one of: titleLangKey, subTitleLangKey', () => {
         if (!titleLangKey && !subTitleLangKey) warnStoryId(story, 'titleLangKey and subTitleLangKey')
         expect(titleLangKey || subTitleLangKey).toBeDefined()
       })
-    })
-  })
-  test('story buttons should be array', () => {
-    stories.map((story) => {
-      story.screens.map((screen) => {
-        const {buttons} = screen
+      test('story buttons should be array', () => {
         if (!buttons || buttons.length === 0) warnStoryId(story, 'button length')
         expect(buttons.length).toBeGreaterThan(0)
       })
-    })
-  })
-  test('story button should have at least one of: langKey, imageUrl', () => {
-    stories.map((story) => {
-      story.screens.map((screen) => {
-        const {buttons} = screen
+      test('story button should have at least one of: langKey, imageUrl', () => {
         buttons.forEach((button: {langKey?: string, imageUrl?: string}) => {
           const {langKey, imageUrl} = button
           if (!langKey && !imageUrl) warnStoryId(story, 'button langKey and imageUrl')
           expect(langKey || imageUrl).toBeDefined()
         })
-      })
-    })
-  })
-  test('story screen should have an existing type', () => {
-    stories.map((story) => {
-      story.screens.map((screen) => {
-        const {type} = screen
-        if (!type || STORYSCREEN_TYPES.indexOf(type) === -1) warnStoryId(story, 'type')
-        expect(STORYSCREEN_TYPES.indexOf(type)).toBeGreaterThan(-1)
       })
     })
   })
@@ -76,41 +59,42 @@ describe('School tests', () => {
     challanges.map((challange) => ids.push(challange.id))
     expect([...new Set(ids)].length === ids.length).toBeTruthy()
   })
-  test('challanges should have an existing type', () => {
-    challanges.map((challange) => {
-      const {type} = challange
+  challanges.map((challange) => {
+    const {type, screens, products} = challange
+    test('challanges should have an existing type', () => {
       if (CHALLANGE_TYPES.indexOf(type) === -1) warnchallangeId(challange, 'type')
       expect(CHALLANGE_TYPES.indexOf(type)).toBeGreaterThan(-1)
     })
-  })
-  test('challanges should have screens or products', () => {
-    challanges.map((challange) => {
-      const {screens, products} = challange
-      if ((!screens || screens.length === 0) && (!products || products.length === 0)) warnchallangeId(challange, 'screens and products')
-      expect(screens || products).toBeDefined()
-    })
-  })
-  test('quiz challenges should have exactly 1 correct answer', () => {
-    challanges.map((challange) => {
-      const {screens} = challange
-      if (screens)
-        screens.map((screen) => {
-          const {buttons} = screen
-          const correctAnwers = buttons.filter((button) => button.isCorrect === true).length
-          if (correctAnwers !== 1) warnchallangeId(challange, 'correct answers')
-          expect(correctAnwers).toBe(1)
+    switch (type) {
+      case 'QUIZ':
+        test('quiz challenges should have screens', () => {
+          if ((!screens || screens.length === 0)) warnchallangeId(challange, 'screens')
+          expect(screens).toBeDefined()
         })
-    })
-  })
-  test('bank challenges price should be Int bigger than 0', () => {
-    challanges.map((challange) => {
-      const {products} = challange
-      if (products)
-        products.map(({price}: {price: number}) => {
-          if (!price || !Number.isInteger(price) || price < 1) warnchallangeId(challange, 'price')
-          expect(!!price && Number.isInteger(price) && price > 0).toBeTruthy()
+        test('quiz challenge screen should have exactly 1 correct answer', () => {
+          if (screens)
+            screens.map((screen) => {
+              const {buttons} = screen
+              const correctAnwers = buttons.filter((button) => button.isCorrect === true).length
+              if (correctAnwers !== 1) warnchallangeId(challange, 'correct answers')
+              expect(correctAnwers).toBe(1)
+            })
         })
-    })
+        break
+      case 'BANK':
+        test('bank challenges should have products', () => {
+          if (!products || products.length === 0) warnchallangeId(challange, 'products')
+          expect(products).toBeDefined()
+        })
+        test('bank challenge product price should be Int bigger than 0', () => {
+          if (products)
+            products.map(({price}: {price: number}) => {
+              if (!price || !Number.isInteger(price) || price < 1) warnchallangeId(challange, 'price')
+              expect(!!price && Number.isInteger(price) && price > 0).toBeTruthy()
+            })
+        })
+        break
+    }
   })
 
   // LESSONS
