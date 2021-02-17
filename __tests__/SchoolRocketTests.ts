cca6f1ee11e7506c622dc277d5258b7e841de00f
Calle Mannervik <c.mannervik@gmail.com>
2021-02-17 13:45:19 +0100

    Added tests for rocket

---
+++ __tests__/SchoolRocketTests.ts
@@ -0,0 +1,60 @@
+import {rockets} from '../school/School'
+jest.disableAutomock()
+
+const warnRocketId = (rocket: { id: number }, prop: string) => {
+  console.warn(`Rocket with id: ${rocket.id} having problems with: ${prop} `)
+}
+
+describe('School rocket tests', () => {
+  test('it should return number of rockets', () => {
+    expect(rockets.length).toBeGreaterThan(0)
+  })
+  test('rockets should have uniqueIDs', () => {
+    let ids: Array<Number> = []
+    rockets.map((rocket) => ids.push(rocket.id))
+    expect([...new Set(ids)].length === ids.length).toBeTruthy()
+  })
+  rockets.map((rocket) => {
+    const {subtitleLangKey, screens} = rocket
+
+    test('rockets should have subtitleLangKey', () => {
+      if ((!subtitleLangKey)) warnRocketId(rocket, 'subtitleLangKey')
+      expect(subtitleLangKey).toBeDefined()
+    })
+
+    test('rockets should have screens', () => {
+      if ((!screens || screens.length === 0)) warnRocketId(rocket, 'screens')
+      expect(screens).toBeDefined()
+    })
+
+    if (screens)
+      screens.forEach((screen) => {
+        const {buttons} = screen
+        test('rockets screen should have buttons', () => {
+          if ((!buttons || buttons.length === 0)) warnRocketId(rocket, 'buttons')
+          expect(buttons).toBeDefined()
+        })
+
+        if (buttons) {
+          test('buttons should have uniqueIDs', () => {
+            let ids: Array<Number> = []
+            buttons.forEach((button) => ids.push(button.id))
+            expect([...new Set(ids)].length === ids.length).toBeTruthy()
+          })
+
+          test('rockets screen should have exactly 1 correct button', () => {
+            const correctAnwers = buttons.filter((button) => button.isCorrect === true).length
+            if (correctAnwers !== 1) warnRocketId(rocket, 'correct answers')
+            expect(correctAnwers).toBe(1)
+          })
+
+          buttons.forEach((button) => {
+            test('buttons should have langKey', () => {
+              if (!button.langKey) warnRocketId(rocket, 'button langKey')
+              expect(button.langKey).toBeDefined()
+            })
+          })
+        }
+      })
+  })
+})
